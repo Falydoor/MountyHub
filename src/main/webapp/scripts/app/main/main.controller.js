b26739e805b445fef06f374b5d74859810c563ed
Théo LEBRUN <mib.ftw@gmail.com>
2016-02-28 00:35:52 -0500

    Button for troll creation

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('mountyhubApp')
-    .controller('MainController', function ($scope, Principal) {
-        Principal.identity().then(function(account) {
+    .controller('MainController', function ($scope, Principal, AddTroll) {
+        Principal.identity().then(function (account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
+
+            $scope.addTroll = function () {
+                AddTroll.query();
+            };
         });
     });
--- src/main/webapp/scripts/components/entities/troll/troll.service.js
+++ src/main/webapp/scripts/components/entities/troll/troll.service.js
@@ -3,7 +3,7 @@
 angular.module('mountyhubApp')
     .factory('Troll', function ($resource, DateUtils) {
         return $resource('api/trolls/:id', {}, {
-            'query': { method: 'GET', isArray: true},
+            'query': {method: 'GET', isArray: true},
             'get': {
                 method: 'GET',
                 transformResponse: function (data) {
@@ -12,6 +12,11 @@ angular.module('mountyhubApp')
                     return data;
                 }
             },
-            'update': { method:'PUT' }
+            'update': {method: 'PUT'}
+        });
+    })
+    .factory('AddTroll', function ($resource, DateUtils) {
+        return $resource('api/addTroll', {}, {
+            'query': {method: 'GET', params: {number: 1, restrictedPassword: 'a'}}
         });
     });

21162fbcfe9983e482df83f3b6826f595bc4e462
Théo LEBRUN <mib.ftw@gmail.com>
2016-03-04 10:04:40 -0500

    Cell color

--- src/main/webapp/scripts/app/monTroll/monProfil/monProfil.controller.js
+++ src/main/webapp/scripts/app/monTroll/monProfil/monProfil.controller.js
@@ -43,7 +43,7 @@ angular.module('mountyhubApp')
             if (v > 0) {
                 return ' success';
             } else if (v < 0) {
-                return 'danger';
+                return ' danger';
             }
         };
 

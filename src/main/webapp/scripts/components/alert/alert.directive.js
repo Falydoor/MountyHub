26351a841c88fa9453756eccc4432bcc9848572e
Théo LEBRUN <mib.ftw@gmail.com>
2016-03-11 09:41:37 -0500

    Alert directive show their corresponding errors

--- src/main/webapp/scripts/components/alert/alert.directive.js
+++ src/main/webapp/scripts/components/alert/alert.directive.js
@@ -5,7 +5,7 @@ angular.module('mountyhubApp')
         return {
             restrict: 'E',
             template: '<div class="alerts" ng-cloak="">' +
-                            '<div ng-repeat="alert in alerts" ng-class="[alert.position, {\'toast\': alert.toast}]">' +
+                            '<div ng-repeat="alert in alerts | filter:{type:\'success\'}" ng-class="[alert.position, {\'toast\': alert.toast}]">' +
                                 '<uib-alert ng-cloak="" type="{{alert.type}}" close="alert.close()"><pre>{{ alert.msg }}</pre></uib-alert>' +
                             '</div>' +
                       '</div>',
@@ -23,7 +23,7 @@ angular.module('mountyhubApp')
         return {
             restrict: 'E',
             template: '<div class="alerts" ng-cloak="">' +
-                            '<div ng-repeat="alert in alerts" ng-class="[alert.position, {\'toast\': alert.toast}]">' +
+                            '<div ng-repeat="alert in alerts | filter:{type:\'danger\'}" ng-class="[alert.position, {\'toast\': alert.toast}]">' +
                                 '<uib-alert ng-cloak="" type="{{alert.type}}" close="alert.close(alerts)"><pre>{{ alert.msg }}</pre></uib-alert>' +
                             '</div>' +
                       '</div>',

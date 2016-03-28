45e46bd8ee8d1f69b9fef3b5b7ec4a7e227893f5
Théo LEBRUN <mib.ftw@gmail.com>
2016-03-28 15:24:07 -0400

    Added SP_Bonusmalus

--- src/main/java/com/mountyhub/app/domain/enumeration/ScriptName.java
+++ src/main/java/com/mountyhub/app/domain/enumeration/ScriptName.java
@@ -4,5 +4,5 @@ package com.mountyhub.app.domain.enumeration;
  * The ScriptName enumeration.
  */
 public enum ScriptName {
-    SP_Caract,SP_Profil2,SP_ProfilPublic2,SP_Equipement,SP_Mouche,SP_Aptitudes2
+    SP_Caract,SP_Profil2,SP_ProfilPublic2,SP_Equipement,SP_Mouche,SP_Aptitudes2,SP_Bonusmalus
 }

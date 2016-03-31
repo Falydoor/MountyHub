c2c40bf5f58151e4c77fc16dc2532b69fe94ccd3
Théo LEBRUN <mib.ftw@gmail.com>
2016-03-31 15:13:34 -0400

    Removed useless filter

--- src/main/webapp/scripts/components/util/profil.filter.js
+++
@@ -1,6 +0,0 @@
-angular.module('mountyhubApp')
-    .filter('capitalize', function () {
-        return function (input) {
-            return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
-        }
-    });
